b911af2ecf548b8399d768d29037c8859608d30c
Indrajith V <indrajithvgp@yahoo.com>
2021-05-21 19:32:21 +0530

    quick issue fix

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -23,6 +23,13 @@ const Subscription = {
 
             return pubsub.asyncIterator(`comment ${postId}`)
         }
+    },
+    post:{
+        subscribe(parent, args, {db, pubsub}, info){
+
+
+            return pubsub.asyncIterator('post')
+        }
     }
 }
 
